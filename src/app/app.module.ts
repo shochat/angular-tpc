b97486f9fed6ed4f0bcfde9cea1a6c7e7b35e1ad
ilan.shochat@gmail.com <yuvik2004>
2017-05-25 07:50:24 +0300

    Init page layout and add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MdInputModule, MdOptionModule, MdSelectModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { LeftSidebarComponent } from './layout/left-sidebar/left-sidebar.component';
+import { HomeComponent } from './home/home.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RightSidebarComponent } from './layout/right-sidebar/right-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LeftSidebarComponent,
+    HomeComponent,
+    RightSidebarComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdInputModule,
+    MdOptionModule,
+    MdSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
