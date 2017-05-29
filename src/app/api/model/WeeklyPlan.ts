5263832eec6522ca96bbeb7936f4ae104a7ab98e
ilan.shochat@gmail.com <yuvik2004>
2017-05-29 08:49:53 +0300

    Add elements to model

---
+++ src/app/api/model/WeeklyPlan.ts
@@ -0,0 +1,7 @@
+import {Workout} from './Workout';
+export class WeeklyPlan {
+  private weeklyTotal: number;
+  private isRecovery: boolean;
+  private isBLevelRace: boolean;
+  private workouts: Workout[];
+}
---
+++ src/app/api/model/Workout.ts
@@ -0,0 +1,7 @@
+export class Workout {
+  private description: String;
+  private duration: number;
+  private length: number;
+  private type: number;
+  private day: number;
+}
