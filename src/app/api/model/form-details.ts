5fc07c4e45e2f97aa3dbebbc1420a599bd7f7ba9
Ilan Shochat <ishochat@cisco.com>
2017-05-30 15:48:43 +0300

    Make members public without constructor

--- src/app/api/model/form-details.ts
+++ src/app/api/model/form-details.ts
@@ -1,11 +1,10 @@
 import {RaceDetails} from './race-details';
 export class FormDetails {
-  constructor(
-    public raceDetails: RaceDetails,
-    public age: number,
-    public weight: number,
-    public shapeLevel: number,
-    public weeklyWorkoutDays: number,
-    public racingHistory?: RaceDetails[]
-  ) {}
+    public raceDetails: RaceDetails;
+    public age: number;
+    public weight: number;
+    public shapeLevel: number;
+    public weeklyWorkoutDays: number;
+    public racingHistory?: RaceDetails[];
+  constructor() {}
 }
