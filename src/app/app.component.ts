8ba966b205beae57403dd16c94f2f0a7ada4d2f0
ilan.shochat@gmail.com <yuvik2004>
2017-06-03 22:01:41 +0300

    Move basic template to .ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <app-header></app-header>
+      <div id="main">
+        <router-outlet></router-outlet>
+        <!--<app-left-sidebar></app-left-sidebar>-->
+        <!--<app-right-sidebar></app-right-sidebar>-->
+      </div>
+      <app-footer></app-footer>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
 }
